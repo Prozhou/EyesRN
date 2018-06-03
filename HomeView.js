ff80155fe918ac79c40a46cdbb19470688a82da2
zhouchao@sidechef.com <zhouchao@sidechef.com>
2018-06-04 06:31:16 +0800

    init

---
+++ HomeView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {
+    View,
+    Text,
+} from 'react-native'
+
+const HomeView =()=>(
+    <View style={{backgroundColor: red}}>
+        <Text>helloworld!</Text>
+    </View>
+)
